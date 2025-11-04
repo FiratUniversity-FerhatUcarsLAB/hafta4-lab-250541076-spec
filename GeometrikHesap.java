73556d65af45fd54151698f683e65ea96898fed2
nur <250541076@firat.edu.tr>
2025-11-04 16:03:23 +0300

    Correct geometric calculations in GeometrikHesap
    Fix errors in geometric calculations and variable definitions.

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -8,38 +8,4 @@
  *hacmini ve yüzey alanını hesaplayıp ekrana 2 ondalıklı biçimde yazdırır.
  */
 
-import java.util.Scanner;
-
-public class GeometrikHesap {
-  public static void main (String[] args) {
-
-    //Pi sayısını sabit olarak tanımla
-    final souble Pi = 3.14159;
-
-    //Değişken tanımlamaları
-    double yaricap;
-    double daireAlani, daireCevresi, daireCapi;
-    double kureHacmi, kureYuzeyAlani;
-
-    //Kullanıcıdan yarıçap değerini al
-    Scanner input = new Scanner(System.in);
-    System.out.println("=== GEOMETRIK HESAPLAYICI ===");
-    System.out.print("Dairenin yaricapini girin (cm): ");
-    yaricap = input.nextDouble();
-
-    //Hesaplamalar
-    daireAlani = Pi * yaricap * yaricap;
-    daireCevresi = 2 * Pi * yaricap;
-    kureHacmi = 2 * yaricap;
-    kureYuzeyAlani = 4 * Pi * yaricap * yaricap;
-
-    //Sonuçları yazdır
-    System.out.println("\nSONUÇLAR:");
-    System.out.printf("Daire Alani      : %.2f cm²%n", daireAlani);
-    System.out.printf("Daire Cevresi    : %.2f cm%n, daireCevresi);
-    System.out.printf("Kure Hacmi       : %.2f cm%n, kureHacmi);
-    System.out.printf("Kure Yuzey Alani : %.2f cm²%n, KureYuzeyAlani);
-
-    input.close();
-  }
-}
+ 
